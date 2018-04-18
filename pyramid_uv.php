3ad0aa1e7350742ed9fbf90b500dbdf21460d96a
TMP System User <tmplinux@localhost.localdomain>
2018-04-18 13:57:14 -0400

    Added UV Pyramid

---
+++ pyramid_uv.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html><html><head>
+  <title>Newington, CT Air Quality &amp; Pollen | Weather Underground</title>
+</head>
+<body class="omnibus page">
+
+
+
+<uv-index-pyramid _ngcontent-c9="" _nghost-c18="">
+   <div _ngcontent-c18="" class="pyramid">
+      <!---->
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk"></div>
+      <div _ngcontent-c18="" class="chunk moderate"></div>
+      <div _ngcontent-c18="" class="chunk moderate"></div>
+      <div _ngcontent-c18="" class="chunk low"></div>
+      <div _ngcontent-c18="" class="chunk low"></div>
+   </div>
+</uv-index-pyramid>
+
+<style>
+
+body.omnibus.page{
+     padding: 30px;
+}
+ html{
+     font-size:100%;
+     box-sizing:border-box
+}
+/*! CSS Used from: Embedded */
+ .pyramid[_ngcontent-c18]{
+    width:232px;
+    margin:20px auto;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]{
+    border-bottom:14px solid #D7D7D7;
+    border-left:8px solid transparent;
+    border-right:8px solid transparent;
+    height:0;
+    width:232px;
+    margin:3px auto;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(1){
+    width:16px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(2){
+    width:34px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(3){
+    width:52px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(4){
+    width:70px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(5){
+    width:88px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(6){
+    width:106px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(7){
+    width:124px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(8){
+    width:142px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(9){
+    width:160px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(10){
+    width:178px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(11){
+    width:196px;
+}
+ .pyramid[_ngcontent-c18] .chunk[_ngcontent-c18]:nth-child(12){
+    width:214px;
+}
+ .pyramid[_ngcontent-c18] .chunk.low[_ngcontent-c18]{
+    border-bottom-color:#5B9F49;
+}
+ .pyramid[_ngcontent-c18] .chunk.moderate[_ngcontent-c18]{
+    border-bottom-color:#FFB92F;
+}
+ *,:after,:before{
+    box-sizing:inherit;
+}
+ div{
+    margin:0;
+    padding:0;
+}
+ @media print{
+     *{
+        background:transparent!important;
+        color:#000!important;
+        box-shadow:none!important;
+        text-shadow:none!important;
+    }
+}
+ :focus{
+    outline:none!important;
+}
+
+
+</style>
+
+
+
+
+
+</body>
+</head>
