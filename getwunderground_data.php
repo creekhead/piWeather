e76b9cb87663f7954f0d42852d9d353d816149ff
creekhead <creekhead@yahoo.com>
2018-12-07 20:31:24 -0500

    Update getwunderground_data.php
    Added garbage collection

--- getwunderground_data.php
+++ getwunderground_data.php
@@ -1,12 +1,11 @@
 <?php
 include "database.php";
 
-getRawWeatherData_tosql();exit;
-
-getRawWeatherData_fromsql();
+getRawWeatherData_tosql();
 
 exit;
 
+//DEBUG
 $parsed_json = json_decode($json_string);
 $location = $parsed_json->location->city;
 $weather =$parsed_json->current_observation->weather;
@@ -15,52 +14,27 @@ echo "Current temperature in ${location} is: ${temp_c}\n degrees and it is curre
 
 
 
-function getRawWeatherData_fromsql(){
-
+function getRawWeatherData_tosql(){
+	date_default_timezone_set('America/New_York');
 	$con = new mysqli(DbSettings::$Address,DbSettings::$Username,DbSettings::$Password,DbSettings::$Schema);
 
-	if (mysqli_connect_errno()) {
-	    die('Error conecting to DB');
-	}else{
-		$sql = "SELECT * FROM wunder_json
-				ORDER BY TIMESTAMP DESC
-				LIMIT 1;";
+	//garbage collection
+	$hour=date('H');
+	$minutes=date('m');
+ 
+	//between 11:30 and midnight truncate table
+	if($hour==23 && ($minutes>30 && $minutes<59)){
+		$result = $con->query("SELECT COUNT(*) as count FROM wunder_json");
+		$current_row_count = $result->fetch_object()->count;
 
-		$result = $con->query($sql);
-
-		if ($result->num_rows > 0) {
-		    // output data of each row
-		    while($row = $result->fetch_assoc()) {
-		    	$wunder_json=unserialize($row["wunder_json"]);
-		    	$openweather_json=unserialize($row["openweather_json"]);
-
-		    	$parsed_json = json_decode($wunder_json);
-		    	echo "<BR>VAR: <b>$ parsed_json: </b><PRE>";
-		    	print_r($parsed_json);
-		    	print_r('<br><br/>Variable: parsed_json - /c/users/johnny/appdata/local/temp/rsub-wah9iq/getwunderground_data.php:38');
-		    	exit;
-				$location = $parsed_json->location->city;
-				$weather =$parsed_json->current_observation->weather;
-				$temp_c = $parsed_json->current_observation->temp_c;
-				echo "Current temperature in ${location} is: ${temp_c}\n degrees and it is currently ${weather}";
-
-		    	echo "<PRE>";
-		    	print_r($wunder_json);
-		    	echo '<hr style="color:tomato">';
-		    	print_r($openweather_json);
-		    	exit;
-		        echo "wunder_json: " . $row["wunder_json"]. " - openweather_json: " . $row["openweather_json"]. "<br>";
-		    }
-		} else {
-		    echo "0 results";
+		if($current_row_count>20){
+			$sql="TRUNCATE TABLE wunder_json";
+			if ($con->query($sql) === TRUE) {
+		   		echo "Table truncated!<br>";
+			}
 		}
-	}
+  	}
 
-	$con->close();
-}
-
-function getRawWeatherData_tosql(){
-	//$json_wunderground = file_get_contents("http://api.wunderground.com/api/a4b1e907bb43a8dc/conditions/q/CT/Newington.json");
 	$json_wunderground = file_get_contents("http://api.wunderground.com/api/a4b1e907bb43a8dc/conditions/astronomy/q/CT/Newington.json");
 
 	$json_wunderground_all = file_get_contents("http://api.wunderground.com/api/a4b1e907bb43a8dc/conditions/astronomy/hourly/forecast/forecast10day/almanac/alerts/currenthurricane/satellite/q/CT/Newington.json");
@@ -68,14 +42,12 @@ function getRawWeatherData_tosql(){
 	$json_openweather = file_get_contents("http://api.openweathermap.org/data/2.5/weather?lat=41.68593&lon=-72.71117&appid=5c507080d1448544b56d307f400bdeee&mode=json&units=imperial&type=accurate");
 
 	$json_openweather_forecast = file_get_contents("http://api.openweathermap.org/data/2.5/forecast?lat=41.68593&lon=-72.71117&appid=5c507080d1448544b56d307f400bdeee&mode=json&units=imperial&type=accurate");
-
-	//$json_wunderground='testdata';
-	//$json_openweather='testdata';
+ 
 	$json_wunderground_serialized=serialize($json_wunderground);
 	$json_wunderground_serialized_all=$json_wunderground_all;
 	$json_openweather_serialized=serialize($json_openweather);
 	$json_openweather_forecast_serialized=serialize($json_openweather_forecast);
-	$con = new mysqli(DbSettings::$Address,DbSettings::$Username,DbSettings::$Password,DbSettings::$Schema);
+
 
 	if (mysqli_connect_errno()) {
 	    die('Error conecting to DB');
@@ -84,10 +56,11 @@ function getRawWeatherData_tosql(){
 
 		if ($con->query($sql) === TRUE) {
 	   		echo "New record created successfully";
-		} else {
-		    echo "Error: " . $sql . "<br>" . $con->error;
-		}
+		} 
 	}
+
+
+
 
 	$con->close();
 }
